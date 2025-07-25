8a7e9b0c3ee4eee8bdc2484be55c4f84dec3d5e4
Dondo_ie19@student.itstep.org <Dondo_ie19@student.itstep.org>
2025-07-25 16:21:10 +0300

    pochinit 2 slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Page1 } from './components/page1/page1'
+import { Page2 } from './components/page2/page2'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import { useEffect } from 'react'
+
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
     <>
       <div className='main-components'>
         <Page1 />
+        <Page2 />
       </div>
     </>
   )
--- src/components/page2/page2.tsx
+++ src/components/page2/page2.tsx
@@ -1,16 +1,61 @@
-import './page1.css';
+import './page2.css';
 
-export function Page1() {
+export function Page2() {
     return (
-        <div className="skyline-container">
+        <div className="page2-skyline-container">
             <img
                 src="img/frame.png"
                 alt="Frame Background"
-                className="skyline-background"
+                className="page2-skyline-background"
                 data-aos="fade-up"
                 data-aos-duration="500"
             />
+            <div className="page2-image-background">
+                <img src="./public/backgrounds/background2slide.jpg" alt="Background" />
+                </div>
 
+            <div className="page2-columns">
+                <div className="page2-column">
+                    <h2 className="page2-title">Чому ми?</h2>
+                    <div className="page2-item">
+                        <img src="./public/icons/1.png" alt="icon" />
+                        <p>Ми — бойове R&D-ком’юніті з фронтовим досвідом.</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/2.png" alt="icon" />
+                        <p>Створюємо засіб разом з екіпажами під реальні ТЗ.</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/3.png" alt="icon" />
+                        <p>Комплекс працює одразу — без донастройки.</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/4.png" alt="icon" />
+                        <p>Свій для своїх<br />Тут немає клієнтів — тут партнери</p>
+                    </div>
+                </div>
+
+                <div className="page2-column">
+                    <h2 className="page2-title">Яку біль ми знімаємо?</h2>
+                    <div className="page2-item">
+                        <img src="./public/icons/5.png" alt="icon" />
+                        <p>Підрозділ отримує саме той засіб, що йому потрібен</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/6.png" alt="icon" />
+                        <p>Дрони вже адаптовані до умов, частот, батарей</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/7.png" alt="icon" />
+                        <p>Комплекс готовий до застосування з коробки</p>
+                    </div>
+                    <div className="page2-item">
+                        <img src="./public/icons/8.png" alt="icon" />
+                        <p>Жодного «допилювання» — лише результат</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 }
