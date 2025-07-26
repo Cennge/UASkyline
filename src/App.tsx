5d313f0a3c7d2516d06f0e8aa1e9617e69db7a09
Ilya <ilyagliba02@gmail.com>
2025-07-26 18:41:23 +0300

    page 4 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Page1 } from './components/page1/page1'
 import { Page2 } from './components/page2/page2'
 import { Page3 } from './components/page3/page3'
 import { Page4 } from './components/page4/page4'
-import { Page5 } from './components/page5/page5'
+// import { Page5 } from './components/page5/page5'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -23,7 +23,7 @@ function App() {
         <Page2 />
         <Page3 />
         <Page4 />
-        <Page5 />
+        {/* <Page5 /> */}
       </div>
     </>
   )
