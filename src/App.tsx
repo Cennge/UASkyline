b86b9e708c56b448a08a9ca687743c013126a1c0
Dondo_ie19@student.itstep.org <Dondo_ie19@student.itstep.org>
2025-07-26 16:41:02 +0300

    adaptive for page4 + page5 started

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Page1 } from './components/page1/page1'
 import { Page2 } from './components/page2/page2'
 import { Page3 } from './components/page3/page3'
 import { Page4 } from './components/page4/page4'
+import { Page5 } from './components/page5/page5'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -22,6 +23,7 @@ function App() {
         <Page2 />
         <Page3 />
         <Page4 />
+        <Page5 />
       </div>
     </>
   )
---
+++ src/components/page5/page5.tsx
@@ -0,0 +1,22 @@
+import './page5.css';
+
+export function Page5() {
+    return (
+        <div className="page5-skyline-container">
+            <img
+                src="img/frame.png"
+                alt="Frame Background"
+                className="page5-skyline-background"
+                data-aos="fade-up"
+                data-aos-duration="500"
+            />
+            <div className='page5-skyline-content'>
+                <div className='page5-left-side-drone'>
+                    <p>SKIF-10D</p>
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
