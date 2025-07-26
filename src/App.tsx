399c37e377e1d8f2231ee454370c2d3f72950824
Dondo_ie19@student.itstep.org <Dondo_ie19@student.itstep.org>
2025-07-26 18:32:39 +0300

    page16 DONE

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { Page1 } from './components/page1/page1'
+import Page16 from './components/page16/page16'
 import { Page2 } from './components/page2/page2'
 import { Page3 } from './components/page3/page3'
 import { Page4 } from './components/page4/page4'
 import { Page5 } from './components/page5/page5'
+
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
@@ -24,6 +26,7 @@ function App() {
         <Page3 />
         <Page4 />
         <Page5 />
+        <Page16/>
       </div>
     </>
   )
