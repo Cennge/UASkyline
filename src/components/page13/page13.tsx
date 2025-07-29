268377c0ec9f66ab80370d71993778ae1e2955bf
Dondo_ie19@student.itstep.org <Dondo_ie19@student.itstep.org>
2025-07-30 01:42:55 +0300

    DONE

--- src/components/page13/page13.tsx
+++ src/components/page13/page13.tsx
@@ -17,8 +17,17 @@ export function Page13() {
         { parameter: "6S4P", value: "3800" },
     ];
 
-    const renderTable = (title: string, data: { parameter: string; value: string }[]) => (
-        <div className="page13-mini-table">
+    const renderTable = (
+        title: string,
+        data: { parameter: string; value: string }[],
+        delay: number
+    ) => (
+        <div
+            className="page13-mini-table"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay={delay}
+        >
             <div className="page13-table-title">{title}</div>
             <div className="page13-table-content">
                 {data.map((row, index) => (
@@ -37,30 +46,121 @@ export function Page13() {
                 src="img/frame.png"
                 alt="Frame Background"
                 className="page13-skyline-background"
+                data-aos="fade-in"
+                data-aos-duration="1000"
+                data-aos-delay={0}
+            />
+            <img
+                className="page13-madeinua-icon"
+                src="./public/img/madeInUa.jpg"
+                alt="Made in UA"
+                data-aos="fade-down-left"
+                data-aos-duration="1000"
+                data-aos-delay={100}
+            />
+            <img
+                className="page13-bottom-image"
+                data-aos="fade-up-left"
+                data-aos-duration="1000"
+                data-aos-delay={200}
+                src="./public/logo/UASkyline_logo8.png"
+                alt="Logo"
             />
 
-            <div className="page13-inner">
-                <div className="page13-left">
-                    <div className="page13-title">АКУМУЛЯТОР ДЛЯ ДРОНІВ</div>
-                    <div className="page13-subtitle">ЗАБЕЗПЕЧУЄ ТРИВАЛУ РОБОТУ ТА НАДІЙНІСТЬ</div>
-                    <div className="page13-battery-image-container">
-                        <img
-                            src="./public/drones/drone_battery.png"
-                            alt="Battery"
-                            className="page13-battery-image"
-                        />
+            <div
+                className="page13-inner"
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                data-aos-delay={300}
+            >
+                <div
+                    className="page13-left"
+                    data-aos="fade-right"
+                    data-aos-duration="1000"
+                    data-aos-delay={400}
+                >
+                    <div className="page13-left-frame">
+                        <div className="page13-left-content">
+                            <div
+                                className="page13-title"
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"
+                                data-aos-delay={500}
+                            >
+                                АКУМУЛЯТОР ДЛЯ ДРОНІВ
+                            </div>
+                            <div
+                                className="page13-subtitle"
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"
+                                data-aos-delay={600}
+                            >
+                                ЗАБЕЗПЕЧУЄ ТРИВАЛУ РОБОТУ ТА НАДІЙНІСТЬ
+                            </div>
+                            <div
+                                className="page13-battery-image-container"
+                                data-aos="zoom-in"
+                                data-aos-duration="1000"
+                                data-aos-delay={700}
+                            >
+                                <img
+                                    src="./public/drones/drone_battery.png"
+                                    alt="Battery"
+                                    className="page13-battery-image"
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="page13-right">
-                    {renderTable("Ціни на елементах Molicel P42A, грн", data1)}
-                    {renderTable("Ціни на АКБ на елементах Samsung 50S, грн", data2)}
-                    {renderTable("Ціни на АКБ на елементах Quantum 4000mah, грн", data3)}
+                <div
+                    className="page13-right"
+                    data-aos="fade-left"
+                    data-aos-duration="1000"
+                    data-aos-delay={800}
+                >
+                    <div className="page13-table-row-wrapper">
+                        {renderTable("Ціни на елементах Molicel P42A, грн", data1, 900)}
+                        {renderTable("Ціни на АКБ на елементах Samsung 50S, грн", data2, 1000)}
+                        {renderTable(
+                            "Ціни на АКБ на елементах Quantum 4000mAh, грн",
+                            data3,
+                            1100
+                        )}
+                    </div>
+
+                    <div
+                        className="page13-description"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay={1200}
+                    >
+                        Розробляємо та виготовляємо АКБ під літальні апарати, FPV-бомбері та НРК.
+                        Працюємо під ТЗ замовника.
+                    </div>
+
+                    <div
+                        className="page13-bottom-battery"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay={1300}
+                    >
+                        <img src="./public/drones/2battery.png" alt="Battery Model" />
+                        <div className="page13-bottom-caption">
+                            Під замовленням відповідно до Вашого ТЗ
+                        </div>
+                    </div>
+
+                    <div
+                        className="page13-dronetypes"
+                        data-aos="fade-up"
+                        data-aos-duration="1000"
+                        data-aos-delay={1400}
+                    >
+                        ДЛЯ БПЛА Вампір / Баба Яга / HeavyShot / Кажан / Немезіс / Агродрон
+                    </div>
                 </div>
             </div>
-
-            <img className="page13-madeinua-icon" src="./public/img/madeInUa.jpg" alt="Made in UA" />
-            <img className="page13-bottom-image" src="./public/logo/UASkyline_logo8.png" alt="Logo" />
         </div>
     );
 }
